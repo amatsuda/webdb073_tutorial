f2e8f5a06315f927af95c2d86aa00e687880f9b7
Akira Matsuda <ronnie@dio.jp>
2013-02-15 18:34:34 +0900

    Permit newly added published_on attribute

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :price)
+      params.require(:book).permit(:title, :price, :published_on)
     end
 end
