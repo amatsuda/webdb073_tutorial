9d35a875c447e6e7b5a8752ac428505d1a03c29a
Akira Matsuda <ronnie@dio.jp>
2013-02-15 18:30:31 +0900

    root to: books#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Webdb073Tutorial::Application.routes.draw do
   resources :books
 
+  root 'books#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
