e837f9d86719e926b131368aeefc0af096965c5a
Akira Matsuda <ronnie@dio.jp>
2013-02-15 15:57:53 +0900

    rails new webdb073_tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webdb073Tutorial::Application.config.secret_key_base = 'c62fb9babcbfe51274bd6b5a0a06d65e50bb676e84db1cc124efc7f1eb94b31464299a54e6ca98f24ebd592d3cb53a9fbfb3f32244bcb5a3f54cd9bf7a5e0d4e'
